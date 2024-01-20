c643726c752d82d365d420f2f02224a589342602
miquaellog <miquaelbueno384385@gmail.com>
2024-01-20 16:01:50 -0300

    soma de um numero e alerta de outro numero

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+ function validAge(){
+ let age = +prompt("digite sua idade");
 
-let  base = +prompt("digite a base")
-let  altura = +prompt ("digite a altura")
 
-console.log(`a base é ${base}`);
-console.log(`a aultura e ${altura}`);
-console.log (`a area do retangulo é ${base*altura}`);
+ function onNumber (){
+ let number1 = +prompt("digite o primeiro numero")
+ let number2 = +prompt("digite o segundo numero")
+
+ if (number1 > 10 && number2 > 10) {
+    alert('os dois numeros sao maiores que 10')
+ } else if (number1 > 10 || number2 > 10 ) {
+    alert("um dos numeros e maior que 10")
+ } else {
+    alert("nenhum dos dois sao maiores que 10")
+     }
+  }
+ if(age < 4){
+     alert("A pessoa e muito nova para o brinquedo");
+  
+  }else if(age > 12){
+     alert ("A pessoa e muito velha para o brinquedo")
+  }else{
+     alert("A pessoa pode brincar no brinquedo")
+    }
+ }
+
+ let count = 0
+ function ofNumber(){
+   
+
+ }
+
 
