9da1b5e660550f80a76c4e46114d49c040225ee9
miquaellog <miquaelbueno384385@gmail.com>
2024-01-27 15:53:42 -0300

    mikael

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
- function validAge(){
- let age = +prompt("digite sua idade");
+//function validAge(){
+//let age = +prompt("digite sua idade");
 
 
- function onNumber (){
- let number1 = +prompt("digite o primeiro numero")
- let number2 = +prompt("digite o segundo numero")
+//function onNumber (){
+//let number1 = +prompt("digite o primeiro numero")
+//let number2 = +prompt("digite o segundo numero")
 
- if (number1 > 10 && number2 > 10) {
-    alert('os dois numeros sao maiores que 10')
- } else if (number1 > 10 || number2 > 10 ) {
-    alert("um dos numeros e maior que 10")
- } else {
-    alert("nenhum dos dois sao maiores que 10")
-     }
-  }
- if(age < 4){
-     alert("A pessoa e muito nova para o brinquedo");
-  
-  }else if(age > 12){
-     alert ("A pessoa e muito velha para o brinquedo")
-  }else{
-     alert("A pessoa pode brincar no brinquedo")
-    }
- }
+//if (number1 > 10 && number2 > 10) {
+//   alert('os dois numeros sao maiores que 10')
+// } else if (number1 > 10 || number2 > 10 ) {
+//   alert("um dos numeros e maior que 10")
+// } else {
+//   alert("nenhum dos dois sao maiores que 10")
+//   }
+// }
+// if(age < 4){
+//    alert("A pessoa e muito nova para o brinquedo");
 
- let count = 0
- function ofNumber(){
-   
+// }else if(age > 12){
+//   alert ("A pessoa e muito velha para o brinquedo")
+// }else{
+//    alert("A pessoa pode brincar no brinquedo")
+//  }
+//}
 
- }
+//let count = 0
+//function ofNumber(){
 
 
+//}
+
+let user = ["miquael", 16]
+
+console.log(user.length)
+ function accessIndex() {
+   let index = +prompt(`digite o index que voçe quer acessar de (0 a ${user.length -1})`)
+   alert (`o elemento de index ${index} e ${user[index]}.O primeiro elemento desse array é ${user[0]}. e o ultimo elemento é ${user[user.legth -1]} `);
+}
+
+function Helloworld10(){
+   for (let contador = 1; contador <= 10; contador = contador + 1) {
+      console.log("Hello, world")
+
+   }
+}
